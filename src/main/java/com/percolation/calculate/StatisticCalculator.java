56ca96997692e857f4685ac5a1ad054482be8da7
Nikita Govokhin <destra1n1337@gmail.com>
2019-05-06 09:22:48 +0400

    test

--- src/main/java/com/percolation/calculate/StatisticCalculator.java
+++ src/main/java/com/percolation/calculate/StatisticCalculator.java
@@ -127,6 +127,8 @@ public class StatisticCalculator {
         result.setAverageAmountClusters(avgAmountClusters);
         result.setAverageSizeCluster(avgSizeCluster);
 
+
+        
         return result;
     }
 }
